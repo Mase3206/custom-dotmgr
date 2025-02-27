ce3b288145c1daa7d2668432235629112a9af702
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2025-02-26 21:51:28 -0700

    allow forceful deletion, equivalence

--- dotmgr/files.py
+++ dotmgr/files.py
@@ -31,14 +31,21 @@ class Dotfile:
 			relative_path (Path) : Relative path to the source file from the dotfiles Git repo. This will also be the relative destination path of the symlink.
 		'''
 		self._prefix = HOME / PREFIX
+		self._relative_path = relative_path
 		# self.repo_src = dotfiles_repo / relative_path
 		self.real_file = self._prefix / relative_path
 		assert self.real_file.exists(), f'{self.real_file} must exist first!'
 		self.link_dest = HOME / relative_path
 
-	def rm_from_home(self):
+	def rm_from_home(self, force = False):
 		'''
 		Remove the existing file or symlink from the user's home folder. Regular files are backed up instead of deleted.
+
+		Attempting to remove a directory will throw an `IsDirectoryError`. To remove it anyway, call with force=True.
+
+		Arguments
+		---------
+			force (bool) : Forcefully remove the targeted file (or directory, which is usually blocked). Defaults to `False`.
 		'''
 		# path to existing (or symlinked) dotfiles in ~/
 
@@ -54,7 +61,11 @@ class Dotfile:
 			self.link_dest.unlink()  # removes the link
 
 		elif self.link_dest.is_dir():
-			raise IsDirectoryError(f'Path {str(self.link_dest)} is a directory, refusing to remove.')
+			if force:
+				out.warn(f'Path {str(self.link_dest)} is a directory, but force=True. Removing {self.link_dest}')
+				shutil.rmtree(self.link_dest)
+			else:
+				raise IsDirectoryError(f'Path {str(self.link_dest)} is a directory, refusing to remove.')
 
 		elif not self.link_dest.exists:
 			# raise FileNotFoundError(f'File {str(self.link_dest)} does not exist.')
@@ -103,6 +114,13 @@ class Dotfile:
 	def __str__(self) -> str:
 		return f'Symlink {self.link_dest} to {self.real_file}'
 	
-
 	def __repr__(self) -> str:
 		return f'Dotfile(real_file=Path({str(self.real_file)}), link_dest=Path({str(self.link_dest)}))'
+	
+	def __eq__(self, other: object) -> bool:
+		if isinstance(other, Dotfile):
+			return self._relative_path == other._relative_path
+		elif isinstance(other, str):
+			return self._relative_path == other
+		else:
+			raise TypeError('Dotfiles can only be compared to other Dotfiles or relative string paths.')
